258ecc7ca5bbe889134c5b7321c5dc6da0e57e91
gustavoyanco <gyanco@gmail.com>
2022-08-18 10:05:35 -0300

    calculadora modificada

--- calculadora.js
+++ calculadora.js
@@ -5,7 +5,7 @@ let producto = require ('./multiplicar');
 let division = require ('./dividir');
 
 
-console.log('La suma es '+suma(8,4), 'La resta es '+resta(7,4));
+console.log('La suma da como resultado '+suma(8,4), 'la resta es '+resta(7,4));
 
 console.log(producto(7,3));
 
